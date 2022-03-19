c39dd325ee11904b2586038e892496c676849456
Anna <67719118+brasspetals@users.noreply.github.com>
2022-03-19 17:57:40 -0500

    Add form validation and error message

---
+++ script.js
@@ -0,0 +1,27 @@
+const form = document.getElementById("form");
+const email = document.getElementById("email");
+const btn = document.getElementById("btn");
+const error = document.getElementById("error");
+const emailRegex = /^[a-zA-Z][a-zA-Z0-9\-\_\.]+@[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}$/;
+
+function validateEmail(e) {
+  if(email.value.match(emailRegex)){
+    e.preventDefault();
+    form.reset();
+    //fade out error message if displayed
+    error.classList.remove('fadeDown');
+    error.classList.add('fadeUp');
+    setTimeout(function() {
+      error.style.display = 'none';
+    }, 300);
+  } else {
+    e.preventDefault();
+    email.focus();
+    //fade in error message
+    error.style.display = 'block';
+    error.classList.add('fadeDown');
+    error.classList.remove('fadeUp');
+  }
+}
+
+btn.addEventListener("click", validateEmail);
